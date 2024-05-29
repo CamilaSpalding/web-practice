1b1a41413d7372fdbc9daa64482ca5f4a55a3e54
CamilaSpalding <camilaspaldingdg@gmail.com>
2024-05-29 17:04:03 -0300

    se esta modificando el boton de search para tenga un dropdown y muestre la barra de búsqueda

---
+++ web-aicura/espacio-de-prueba/de-prueba.js
@@ -0,0 +1,36 @@
+/* BARRA DE BUSQUEDA ANIMADA 1 - BOTON DE SEARCH */
+
+/* const toggleSearch = () => {
+    const searchForm = document.querySelector('.search-form');
+    const searchButton = document.querySelector('.search-button');
+    const searchInput = document.querySelector('.search-input');
+
+    searchButton.addEventListener('click', () => {
+        searchForm.classList.toggle('active-search');
+    });
+
+    searchInput.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            searchInput.value = '';
+            searchForm.classList.remove('active-search');
+        };
+    });
+
+}
+
+toggleSearch(); */
+
+
+
+/* BARRA DE BUSQUEDA ANIMADA 2 - BOTON DE SEARCH */
+let search = document.querySelector('.search');
+let clear = document.querySelector('.clear')
+
+search.onclick = function() {
+    document.querySelector('.container').classList.toggle('active');
+} 
+
+clear.onclick = function() {
+    document.getElementById('search').value = '';
+}
