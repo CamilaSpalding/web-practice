018b5b897354133b063711d2d593fac9fc9723c9
CamilaSpalding <camilaspaldingdg@gmail.com>
2024-05-30 01:49:07 -0300

    se creo la función para que cambie el color del icono del search bar cuando el usuario tipea

--- web-aicura/script.js
+++ web-aicura/script.js
@@ -1,21 +1,40 @@
 const search = document.getElementById('dropdownSearchMenu'),
     searchBtn = document.getElementById('searchBtn'),
     searchClearBtn = document.getElementById('clearBtn'),
-    searchCloseBtn = document.getElementById('closeBtn');
+    searchCloseBtn = document.getElementById('closeBtn'),
+    searchInput = document.getElementById('searchInput'),
+    searchIcon = document.querySelector('.search-icon-img');
 
-/* Search show */
+
+/* Mostrar el earch */
 searchBtn.addEventListener('click', (/* event */) => {
     /* event.preventDefault(); */
     search.classList.add('show-search')
 });
 
-/* Search hidden */
+/* Esconder el search */
 searchCloseBtn.addEventListener('click', () => {
     search.classList.remove('show-search')
 });
 
 
-/* Clear search input */
+/* Resetear o borrar lo escrito en el search input */
 searchClearBtn.addEventListener('click', () => {
     searchInput.value = '';
 })
+
+
+/* Cambiar el ícono de búsqueda cuando el usuario tipee */
+searchInput.addEventListener('input', () => {
+    console.log('Input event detected');
+    updateSearchIcon();
+});
+
+/* Función para actualizar el ícono de búsqueda */
+const updateSearchIcon = () => {
+    if (searchInput.value.trim() === '') {
+        searchIcon.src = './assets/icons/gray-search-icon.svg';
+    } else {
+        searchIcon.src = './assets/icons/black-search-icon.svg';
+    }
+};
